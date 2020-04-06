3b9b14061bfd1a4b694d61207e4422a5fe8e3a4f
Miroslaw Puczynski <miroslaw@puczynski.org>
2020-04-06 18:35:50 +0200

    finalize exercise

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 /**
+https://pl.spoj.com/problems/RNO_DOD/
+
 RNO_DOD - Proste dodawanie
 Twoim zadaniem jest dodać wszystkie liczby całkowite podane na wejściu.
 
@@ -18,7 +20,20 @@ Output:
 15
 0
 */
+ 
+use std::io::BufRead;
+
 
 fn main() {
-    println!("Hello, world!");
+    let stdin = std::io::stdin();
+    let mut lines = stdin.lock().lines();
+    let n:u8 = lines.next().unwrap().unwrap().parse().unwrap();
+    for i in 0..n {
+        let op_num:u16 = lines.next().unwrap().unwrap().parse().unwrap();
+        let l = lines.next().unwrap().unwrap();
+        let s:i32 = l.split(' ').into_iter().map(|x| -> i32 {x.parse().unwrap()}).sum();
+
+        println!("{:?}", s);
+
+    }
 }
